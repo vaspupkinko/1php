e341a206a0bb430bb603956e1a075ba74b9c90f3
vaspupkinko <42377637+vaspupkinko@users.noreply.github.com>
2019-01-29 12:39:16 +0200

    Update update.php

--- update.php
+++ update.php
@@ -2,12 +2,11 @@
 $local_ver = file_get_contents('./ver.txt');
 $top_ver = file_get_contents('https://vaspupkinko.github.io/1php/ver.txt');
 if ($local_ver == $top_ver){
-	
-	fgets(STDIN);
 	goto exit1;
 } else {
 	unlink('./mine.php');
-	fgets(STDIN);
+	$file = file_get_contents('https://vaspupkinko.github.io/1php/mine.php');
+	file_put_contents('./mine.php', $file);
 	goto exit1;
 }
 exit1:
