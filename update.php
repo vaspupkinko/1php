1d9c3fadd8ecdcb2a0b2bbc5e973d720f399b579
vaspupkinko <42377637+vaspupkinko@users.noreply.github.com>
2019-01-29 12:33:37 +0200

    Update update.php

--- update.php
+++ update.php
@@ -2,8 +2,6 @@
 <?php
 $local_ver = file_get_contents('./ver.txt');
 $top_ver = file_get_contents('https://vaspupkinko.github.io/1php/ver.txt');
-echo "Your version: ".$local_ver. " . \n" ;
-echo "Latest version: ".$top_ver. " . \n" ;
 if ($local_ver == $top_ver){
 	
 	fgets(STDIN);
