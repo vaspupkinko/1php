0e9f2a247bc559d8659a250160663d4620361178
vaspupkinko <42377637+vaspupkinko@users.noreply.github.com>
2019-01-29 12:39:43 +0200

    Delete hhh.php

--- hhh.php
+++
@@ -1,6 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/php
-<?php
-echo "lol";
-$file = file_get_contents('https://vaspupkinko.github.io/1php/mine.php');
-file_put_contents('./2.php', $file);
-echo "lol_end";
